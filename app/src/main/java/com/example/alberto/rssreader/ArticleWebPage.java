12646651d1f1e3da24281630e824ca343bbf470c
DrRek <lucareccia@hotmail.it>
2017-12-10 12:07:10 +0100

    Ora funziona il web browser interno
    avevi dimenticato di settarne uno nuovo prima del loadUrl

--- app/src/main/java/com/example/alberto/rssreader/ArticleWebPage.java
+++ app/src/main/java/com/example/alberto/rssreader/ArticleWebPage.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 /**
  * Created by Alberto on 10/12/2017.
@@ -26,6 +27,7 @@ public class ArticleWebPage extends AppCompatActivity{
         setSupportActionBar(toolbar);
 
         webView = (WebView)findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
         webView.loadUrl(getIntent().getExtras().getString("url"));
 
     }
