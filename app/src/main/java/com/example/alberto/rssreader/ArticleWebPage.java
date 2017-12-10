5b4701abd7a6c1ce8b00cc189203fe58b59648c4
DrRek <lucareccia@hotmail.it>
2017-12-10 11:49:01 +0100

    First commit

---
+++ app/src/main/java/com/example/alberto/rssreader/ArticleWebPage.java
@@ -0,0 +1,32 @@
+package com.example.alberto.rssreader;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.webkit.WebView;
+
+/**
+ * Created by Alberto on 10/12/2017.
+ */
+
+public class ArticleWebPage extends AppCompatActivity{
+
+    Toolbar toolbar;
+    WebView webView;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.webview);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("News");
+        setSupportActionBar(toolbar);
+
+        webView = (WebView)findViewById(R.id.webview);
+        webView.loadUrl(getIntent().getExtras().getString("url"));
+
+    }
+}
---
+++ app/src/main/java/com/example/alberto/rssreader/Interface/ItemClickListener.java
@@ -0,0 +1,11 @@
+package com.example.alberto.rssreader.Interface;
+
+import android.view.View;
+
+/**
+ * Created by Alberto on 09/12/2017.
+ */
+
+public interface ItemClickListener {
+    void onClick(View view, int posisiton, boolean isLongClick);
+}
